a8dd23211c9f6264079bebf8bc4893922edea14c
YugaAdiIrawan <yuga.irawan@ciputralife.com>
2026-05-20 14:42:35 +0700

    chore: menambahkan .gitignore dan menghapus folder agent dari tracking

--- generate_token.go
+++ generate_token.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	secret := []byte("aAFSmO5lDRkWiXj/MjE+KiLdoOjBdxFbHvUwI1gZ7OIH2zzMjTGT3CHkxTzSO8Hr0RaS7nqX/8TjHdYYFtwDHQ==")
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	secret := os.Getenv("JWT_SECRET")
+	secretByte := []byte(secret)
 	claims := jwt.MapClaims{
 		"sub":  "123e4567-e89b-12d3-a456-426614174000",
 		"role": "ADMIN",
 		"exp":  time.Now().Add(time.Hour * 24 * 365).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, _ := token.SignedString(secret)
+	tokenString, _ := token.SignedString(secretByte)
 	fmt.Print(tokenString)
 }
